b771c831f3f69c53e1cd8df2a1d04ef06a74d8ce
eftekher-alam <eftekher@yahoo.com>
2023-01-09 13:16:35 +0600

    3 way of display output in JavaScript

---
+++ JavaScript Output/output.js
@@ -0,0 +1,16 @@
+      //We are learning here how to display someting in javascript
+      //Way-1 | Widely use
+      document.getElementById("demo").innerHTML = "Hello from Eftkeher Alam";
+
+      //Way-2 (Not recomended) | It will remove all containt of page while call in browser console
+      document.write("Hello from write function");
+
+      //Way-3
+      window.alert("Hello from alert");
+
+      //Way-4
+      //You will see the output in browser consloe. It mostly use for debugg
+      console.log("Hello to console")
+
+      //This is not for out put. It will give you printer access
+      window.print();
