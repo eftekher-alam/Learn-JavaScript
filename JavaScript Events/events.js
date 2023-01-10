b30066d6becf5f8ba4aa3a6c1fda7b8273dfe31f
eftekher-alam <eftekher@yahoo.com>
2023-01-10 17:07:15 +0600

    JaveScript Events on clicke

---
+++ JavaScript Events/events.js
@@ -0,0 +1,8 @@
+function show()
+{
+    let name = "Eftkeher alam";
+    let dept = "Computer Science and Engineering";
+    console.log("Hello this is : " + name + "\nMy department is : " + dept);
+    console.log(Date());
+}
+
